5ba94d7a1b8414c93361d928ed5f8dc7f853fa74
Aerthas Veras <dgunderson96@gmail.com>
2020-04-20 21:13:09 -0500

    Forgot to put the toggles file into the release

---
+++ Editor/Toggles.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+public class Toggles {
+
+	bool[] toggles; 
+	string[] headers;
+	List<int> mainFoldouts = new List<int>();
+	List<int> subFoldouts = new List<int>();
+
+	public Toggles(bool[] t, string[] h){
+		toggles = t;
+		headers = h;
+		for (int i = 0; i < h.Length; i++){
+			if (h[i].ToUpper() == h[i])
+				mainFoldouts.Add(i);
+			else
+				subFoldouts.Add(i);
+		}
+	}
+
+	public bool GetState(int index){return toggles[index];}
+	public bool GetState(string index){
+		bool state = false;
+		for (int i = 0; i < headers.Length; i++){
+			if (headers[i].ToLower() == index.ToLower()){
+				state = toggles[i];
+			}
+		}
+		return state;
+	}
+
+	public void SetState(int index, bool state){toggles[index] = state;}
+	public void SetState(string index, bool state){
+		for (int i = 0; i < headers.Length; i++){
+			if (headers[i].ToLower() == index.ToLower()){
+				 toggles[i] = state;
+			}
+		}
+	}
+
+	public int[] GetMain(){return mainFoldouts.ToArray();}
+	public int[] GetSub(){return subFoldouts.ToArray();}
+	public bool[] GetToggles(){return toggles;}
+}
